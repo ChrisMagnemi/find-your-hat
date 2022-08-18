fe46a7c202fe920a02609f455a510072672bc808
Chris Magnemi <chrismagnemi@Chriss-Air.lan>
2022-08-18 12:50:06 -0400

    playGame method with promptUserAction, validateUserAction, updateUserPosition, isUserInbounds, starting to update the field

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const readline = require('readline');
-
 const prompt = require('prompt-sync')({sigint: true});
 
 const hat = '^';
@@ -10,13 +8,13 @@ const pathCharacter = '*';
 class Field {
     constructor(field){
         this._field = field;
-        this._length = field[0].length;
+        this._height = field[0].length;
         this._width = field.length;
         this._userPosition = { row: 0, col: 0 };
     }
 
-    get getLength(){
-        return this._length;
+    get getHeight(){
+        return this._height;
     }
     get getWidth(){
         return this._width;
@@ -24,6 +22,7 @@ class Field {
     get getUserPosition(){
         return this._userPosition;
     }
+    // setter for _userPosition?
 
     print(){
         console.log("--------------------------");
@@ -51,30 +50,68 @@ class Field {
     }
 
     updateUserPosition(userAction){
+        // console.log(" >> Start UpdateUserPosition <<");
         let currPos = this.getUserPosition;
-        console.log("user's current position is: ", currPos);
-        console.log("user will move: ", userAction);
 
+        switch (userAction){
+            case 'u':
+            case 'up':
+                this._userPosition.row = this.getUserPosition.row - 1;
+                break;
+            case 'd':
+            case 'down':
+                this._userPosition.row = this.getUserPosition.row + 1;
+                break;
+            case 'l':
+            case 'left':
+                this._userPosition.col = this.getUserPosition.col - 1;
+                break;
+            case 'r':
+            case 'right':
+                this._userPosition.col = this.getUserPosition.col + 1;
+                break;
+            default:
+                console.log("default case");
+        }
+        // console.log(" >> End updateUserPosition << ");
     }
 
-    handleUserAction(){
+    isUserInbounds(){
+        let userPos = this.getUserPosition;
+
+        if (userPos.row < 0 || userPos.row >= this.getHeight || userPos.col < 0 || userPos.col >= this.getWidth){
+            console.log(" GAME OVER, you are OUT OF BOUNDS.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    playGame(){
         // prompt for user action
-        let userAction = this.promptUserAction();
+        let userAction = this.promptUserAction().toLowerCase();
 
         // validate user input is a move
         if (this.validateUserAction(userAction)){
-            console.log("user input is a valid move");
+            console.log(` -- User Move ( ${userAction} ) is valid -- `);
         } else {
-            console.log("Try again.");
+            console.log(" -- Invalid Move > Try again. -- ");
             this.handleUserAction();
         }
+
         // update user position
         this.updateUserPosition(userAction);
+        console.log(" -- User moved to: ", this.getUserPosition, " -- ");
 
+        // check if user is inbounds
+        if (!this.isUserInbounds()){
+            return;
+        }
 
         // check what type of tile the user lands on
 
-        // update field
+
+        // update field and show to user
 
     }
 
@@ -88,5 +125,4 @@ const myField = new Field([
   ]);
 
 myField.print();
-myField.handleUserAction();
-console.log(myField.getLength);
+myField.playGame();
